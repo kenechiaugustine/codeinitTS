63d7d4daa3e3890ee3b51f8b08afdbbb682fcf33
Kene Arionye <kenechiaugustine@gmail.com>
2022-03-20 13:06:34 +0100

    configurations

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Application } from 'express'
+
+const app: Application = express()
+
+
+
+
+
+
+app.listen(3001, () => console.log(`App running...`));
+
+
+
+console.log('Hello Typescript');
