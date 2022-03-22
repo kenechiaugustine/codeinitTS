9aa493a88f84114be482f307c6e399d8224a0b97
Kene Arionye <kenechiaugustine@gmail.com>
2022-03-23 00:14:59 +0100

    fixed heroku crash 1

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,9 @@ import DatabaseConnection from './utils/db.config'
 
 import app from './app'
 
-const PORT = process.env.PORT || 3001
-
 
 
 
 // Server Start
 
-app.listen(PORT, () => console.log(`Application started on port: ${PORT}...`));
+app.listen(process.env.PORT || 3001, () => console.log(`Application started...`));
