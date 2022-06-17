44155998b7f6f145085451567fd61fa7fd7a3a4a
Kene Arionye <kenechiaugustine@gmail.com>
2022-06-17 16:44:33 +0100

    implemented multer

---
+++ src/utils/uploader.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import multer from 'multer'
+import path from 'path'
+
+const storageEngine = multer.diskStorage({
+    // @ts-ignore
+    destination: (req, file, cb) => {
+        cb(null, './src/uploads')
+    },
+    // @ts-ignore
+    filename: (req, file, cb) => {
+        // cb(null, `${file.originalname}--${Date.now()}`)
+        cb(null, Date.now()+ '--' + file.originalname)
+    }
+})
+
+const uploader = multer({
+    storage: storageEngine
+})
+
+export default uploader
