23a901e33e647daa862a8263336b360becdb88a3
Kene Arionye <kenechiaugustine@gmail.com>
2022-12-06 22:28:06 +0100

    code revamp

--- src/utils/api.response.ts
+++ src/utils/api.response.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from "express";
-export const apiresponse = (statusCode: number, message: String = "", data: any, res: Response,) => {
+import { Response } from "express";
+
+export const apiresponse = (statusCode: number, message: String = "", data: any, res: Response) => {
     let status = ""
-    if (statusCode >= 200 && statusCode < 300) {
-        status = "ok"
-    } else {
-        status = "error"
-    }
+    if (statusCode >= 200 && statusCode < 300) status = "ok"
+    status = "error"
+
     let results = data ? data.length : 0
     // Send Response
     return res.status(statusCode).json({
