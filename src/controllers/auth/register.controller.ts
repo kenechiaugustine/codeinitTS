eda060b1a1e9e0a3feb4a8749e277e26f8fcb905
Kenechukwu Arionye <kenechiaugustine@gmail.com>
2023-01-31 07:26:37 +0100

    format

--- src/controllers/auth/register.controller.ts
+++ src/controllers/auth/register.controller.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { User } from '../../models/user.model';
@@ -5,21 +7,26 @@ import { apiresponse } from '../../utils/api.response';
 import { Email } from '../../utils/Email';
 
 export const register = async (req: Request, res: Response) => {
-    const { email, password, firstName, lastName } = req.body;
-    const user = await User.create({ email, password, firstName, lastName })
-    //@ts-ignore
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-    res.cookie('token', token)
-    // Create Verification URL
-    const url = `${req.protocol}://${req.get('host')}/api/auth/verify-email?token=${token}`
-    // Send Welcome Email
-    await new Email(user, url).send('welcome-email', `Welcome to this Platform`)
-    return apiresponse(201, 'User created successfully', user, res);
-}
+  const { email, password, firstName, lastName } = req.body;
+  const user = await User.create({ email, password, firstName, lastName });
+  //@ts-ignore
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie('token', token);
+  // Create Verification URL
+  const url = `${req.protocol}://${req.get(
+    'host'
+  )}/api/auth/verify-email?token=${token}`;
+  // Send Welcome Email
+  // await new Email(user, url).send('welcome-email', `Welcome to this Platform`)
+  return apiresponse(201, 'User created successfully', user, res);
+};
 
 export const registerWithGoogle = async (req: Request, res: Response) => {
-    // Register users with google
-    return apiresponse(201, 'Register user with Google from this endpoint / func', null, res);
-}
-
-
+  // Register users with google
+  return apiresponse(
+    201,
+    'Register user with Google from this endpoint / func',
+    null,
+    res
+  );
+};
--- src/routes/doc.route.ts
+++ src/routes/doc.route.ts
@@ -1,17 +1,21 @@
-import express, { Router } from 'express'
+/** @format */
+
+import express, { Router } from 'express';
 import * as doc from '../controllers/doc/doc.controller';
+import { validate } from '../middlewares/validate';
+import * as authValidator from '../validators/auth.validators';
 
 const router: Router = express.Router();
 
 // Get all users
-router.get('/users', doc.getAllUsers)
+router.get('/users', doc.getAllUsers);
 // Get one user
-router.get('/users/:id', doc.getOneUser)
+router.get('/users/:id', doc.getOneUser);
 // Create a new user
-router.post('/users', doc.createUser)
+router.post('/users', validate(authValidator.register), doc.createUser);
 // Update a user
-router.put('/users/:id', doc.updateUser)
+router.put('/users/:id', doc.updateUser);
 // Delete a  user
-router.delete('/users/:id', doc.deleteUser)
+router.delete('/users/:id', doc.deleteUser);
 
-export { router as docRouter }
+export { router as docRouter };
