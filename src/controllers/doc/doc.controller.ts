458d89e1259ef3f4be5271acf9e254a11eef6159
Kenechukwu Arionye <kenechiaugustine@gmail.com>
2023-01-31 07:26:16 +0100

    code revamp with default handler

--- src/controllers/doc/doc.controller.ts
+++ src/controllers/doc/doc.controller.ts
@@ -1,46 +1,14 @@
-import { Request, Response, NextFunction } from 'express'
+/** @format */
+
 import { User } from '../../models/user.model';
-import AppError from '../../errors/AppError';
-import AppQueryFeatures from '../../utils/AppQueryFeatures';
-import { apiresponse } from '../../utils/api.response';
+import * as doc from './../default.handler';
 
-export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    let filter = {}
-    if (req.params.id) filter = { user: req.params.id }
-    const features = new AppQueryFeatures(User.find(filter), req.query)
-        .filter()
-        .sort()
-        .limitFields()
-        .paginate()
-    //@ts-ignore
-    const users = await features.query;
-    if (!users || users.length == 0) throw new AppError('No Record found', 400);
-    return apiresponse(200, 'Users Record found', users, res);
-}
+export const getAllUsers = doc.getAll(User);
 
-export const getOneUser = async (req: Request, res: Response) => {
-    let user = await User.findById(req.params.id)
-    if (!user) throw new AppError('No user record found', 400)
-    return apiresponse(200, 'User Record found', user, res);
-}
+export const getOneUser = doc.getOne(User);
 
-export const createUser = async (req: Request, res: Response) => {
-    const newUser = await User.create(req.body)
-    return apiresponse(201, 'User created', newUser, res);
-}
+export const createUser = doc.createOne(User);
 
-export const updateUser = async (req: Request, res: Response) => {
-    let user = await User.findById(req.params.id)
-    if (!user) throw new AppError('No user record found', 400)
-    user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    })
-    return apiresponse(200, 'User updated', user, res);
-}
+export const updateUser = doc.updateOne(User);
 
-export const deleteUser = async (req: Request, res: Response) => {
-    const user = await User.findByIdAndDelete(req.params.id)
-    if (!user) throw new AppError('No user record found', 400)
-    return apiresponse(200, 'User deleted', null, res);
-}
+export const deleteUser = doc.removeOne(User);
